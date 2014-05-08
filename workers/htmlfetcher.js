dc59f77673941eb0977e3edcce0167b8b1d9b252
Matthew Olde <mattolde@gmail.com>
2014-05-08 15:22:39 -0700

    removed server for htmlfetcher.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,11 +1,3 @@
-var http = require("http");
 var archive = require('../helpers/archive-helpers');
 
-var handleRequest = function(){
-  archive.downloadUrls();
-}();
-
-var port = 9080;
-var ip = "127.0.0.1";
-var server = http.createServer(handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
+archive.downloadUrls();
